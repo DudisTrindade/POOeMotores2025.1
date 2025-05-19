0180811929394e80ed2a80c86ca575585b411e28
DudisTrindade <eduarda.trindade@escolar.ifrn.edu.br>
2025-05-19 16:24:46 -0300

    prt 11

--- Saulo2025.1/Assets/MovimentoIInimigo.cs
+++ Saulo2025.1/Assets/MovimentoIInimigo.cs
@@ -1,60 +1,70 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class MovimentoInimigo : MonoBehaviour
 {
+    public GameObject ataqueObject;
     private GameObject _player;
+    
     
     private Rigidbody _rigidbody;
     private float velocidade;
 
     public float raioDeVisao = 10;
     private bool naVisao = false;
-
+    
     private SphereCollider _sphereCollider;
 
     public float distanciaMinima = 1.5f;
-
+    
     void Start()
     {
-        
-        _rigidbody =  gameObject.GetComponent<Rigidbody>();
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
         velocidade = gameObject.GetComponent<Inimigo>().Velocidade();
         _sphereCollider = gameObject.GetComponent<SphereCollider>();
-
+        
         _player = GameObject.FindWithTag("Player");
-
     }
 
+ 
     void Update()
     {
         _sphereCollider.radius = raioDeVisao;
 
-        //if (Vector3.Distance(trasform.position, player.transform.position) < raioDeVisao
-        if (naVisao == true)
+        if (Vector3.Distance(transform.position, _player.transform.position) > distanciaMinima)
         {
-            transform.LookAt(_player.transform.position);
-            transform.position = Vector3.MoveTowards(transform.position,
-                _player.transform.position,
-                velocidade * Time.deltaTime);
+            if (naVisao == true)
+            {
+                transform.LookAt(_player.transform.position);
+                transform.position = Vector3.MoveTowards(transform.position,
+                    _player.transform.position,
+                    velocidade * Time.deltaTime);
+            }
+            ataqueObject.SetActive(false);
         }
+        else
+        {
+            ataqueObject.SetActive(true);
+        }
+
+        Debug.DrawLine(transform.position, _player.transform.position, Color.red);
+        
+    }
+    void OnTriggerStay(Collider colisao)
+    {
+        if (colisao.gameObject.CompareTag("Player"))
+        {
+            naVisao = true;
+        }
+        
     }
 
-    void OnTriggerStay(Collider colisao)
-         {
-             if (colisao.gameObject.CompareTag("Player"))
-             {
-                 naVisao = true;
-             }
-         }
-
-
-    void OnTriggerExit(Collider colisao)
+    private void OnTriggerExit(Collider colisao)
     {
         if (colisao.gameObject.CompareTag("Player"))
         {
             naVisao = false;
         }
     }
-
-
 }
